6ec8be60deb87b5761de69e3717d0614e7544b44
unknown <koechgeraldk@gmail.com>
2024-05-02 17:12:19 +0300

    got comfortable with react useState hook

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function dencrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={dencrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
+
+export default App;
